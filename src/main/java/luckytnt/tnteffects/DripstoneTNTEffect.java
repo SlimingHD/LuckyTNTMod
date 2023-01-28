2d9900585bdffdb612ba643e1548305a1ab23645
SlimingHD <94920232+SlimingHD@users.noreply.github.com>
2023-01-28 12:01:42 +0100

    Update DripstoneTNTEffect.java

--- src/main/java/luckytnt/tnteffects/DripstoneTNTEffect.java
+++ src/main/java/luckytnt/tnteffects/DripstoneTNTEffect.java
@@ -1,11 +1,17 @@
 package luckytnt.tnteffects;
 
 import luckytnt.registry.BlockRegistry;
+import luckytntlib.util.IExplosiveEntity;
 import luckytntlib.util.explosions.PrimedTNTEffect;
 import net.minecraft.world.level.block.Block;
 
 public class DripstoneTNTEffect extends PrimedTNTEffect{
 
+	@Override
+	public void serverExplosion(IExplosiveEntity entity) {
+		
+	}
+	
 	@Override
 	public Block getBlock() {
 		return BlockRegistry.DRIPSTONE_TNT.get();
