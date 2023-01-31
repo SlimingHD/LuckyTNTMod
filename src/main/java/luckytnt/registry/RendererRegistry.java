ef1bff98858d0730a84ba235a8ad19a0cd1e711c
SlimingHD <94920232+SlimingHD@users.noreply.github.com>
2023-01-31 16:06:39 +0100

    kod

--- src/main/java/luckytnt/registry/RendererRegistry.java
+++ src/main/java/luckytnt/registry/RendererRegistry.java
@@ -134,13 +134,10 @@ public class RendererRegistry {
 		event.registerEntityRenderer(EntityRegistry.WITHER_STORM.get(), LTNTRenderer::new);
 		event.registerEntityRenderer(EntityRegistry.LEAPING_TNT.get(), BouncingTNTRenderer::new);
 		event.registerEntityRenderer(EntityRegistry.RUSSIAN_ROULETTE.get(), LTNTRenderer::new);
-<<<<<<< Updated upstream
 		event.registerEntityRenderer(EntityRegistry.KNOCKBACK_TNT.get(), LTNTRenderer::new);
-=======
 	
 		//Dynamite
 		event.registerEntityRenderer(EntityRegistry.DYNAMITE.get(), ThrownItemRenderer::new);
->>>>>>> Stashed changes
 		
 		//Projectiles
 		event.registerEntityRenderer(EntityRegistry.METEOR.get(), LTNTRenderer::new);
