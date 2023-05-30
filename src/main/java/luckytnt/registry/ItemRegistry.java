9dcae25884c9ff13365a8035c1dff1da2e3fd07f
SlimingHD <94920232+SlimingHD@users.noreply.github.com>
2023-05-30 21:03:55 +0200

    1.0

--- src/main/java/luckytnt/registry/ItemRegistry.java
+++ src/main/java/luckytnt/registry/ItemRegistry.java
@@ -168,53 +168,53 @@ public class ItemRegistry {
 	public static final RegistryObject<LTNTMinecartItem> LUCKY_TNT_MINECART = LuckyTNTMod.RH.registerTNTMinecartItem("lucky_tnt_minecart", () -> new LTNTMinecartItem(new Item.Properties(), () -> EntityRegistry.LUCKY_TNT_MINECART), "m", false, true);
 	
 	//Other
-	public static final RegistryObject<Item> NUCLEAR_WASTE = LuckyTNTMod.itemRegistry.register("nuclear_waste", () -> new BlockItem(BlockRegistry.NUCLEAR_WASTE.get(), new Item.Properties()));
-	public static final RegistryObject<Item> RED_CANDY = LuckyTNTMod.itemRegistry.register("red_candy", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 100, 2), 1).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 100, 0), 1).build())));
-	public static final RegistryObject<Item> GREEN_CANDY = LuckyTNTMod.itemRegistry.register("green_candy", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.JUMP, 200, 2), 1).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 2), 1).build())));
-	public static final RegistryObject<Item> BLUE_CANDY = LuckyTNTMod.itemRegistry.register("blue_candy", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.WATER_BREATHING, 1200, 0), 1).effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 500, 2), 1).effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 1200, 0), 0).build())));
-	public static final RegistryObject<Item> PURPLE_CANDY = LuckyTNTMod.itemRegistry.register("purple_candy", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 100, 3), 1).effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 1), 1).build())));
-	public static final RegistryObject<Item> YELLOW_CANDY = LuckyTNTMod.itemRegistry.register("yellow_candy", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED, 2000, 3), 1).build())));;
-	public static final RegistryObject<Item> URANIUM_INGOT = LuckyTNTMod.itemRegistry.register("uranium_ingot", () -> new Item(new Item.Properties()));
-	public static final RegistryObject<Item> ANTIMATTER = LuckyTNTMod.itemRegistry.register("antimatter", () -> new Item(new Item.Properties().rarity(Rarity.EPIC)) {
+	public static final RegistryObject<Item> NUCLEAR_WASTE = LuckyTNTMod.itemRegistry.register("nuclear_waste", () -> new BlockItem(BlockRegistry.NUCLEAR_WASTE.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)));
+	public static final RegistryObject<Item> RED_CANDY = LuckyTNTMod.itemRegistry.register("red_candy", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER).food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 100, 2), 1).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 100, 0), 1).build())));
+	public static final RegistryObject<Item> GREEN_CANDY = LuckyTNTMod.itemRegistry.register("green_candy", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER).food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.JUMP, 200, 2), 1).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 2), 1).build())));
+	public static final RegistryObject<Item> BLUE_CANDY = LuckyTNTMod.itemRegistry.register("blue_candy", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER).food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.WATER_BREATHING, 1200, 0), 1).effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 500, 2), 1).effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 1200, 0), 0).build())));
+	public static final RegistryObject<Item> PURPLE_CANDY = LuckyTNTMod.itemRegistry.register("purple_candy", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER).food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 100, 3), 1).effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 1), 1).build())));
+	public static final RegistryObject<Item> YELLOW_CANDY = LuckyTNTMod.itemRegistry.register("yellow_candy", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER).food(new FoodProperties.Builder().alwaysEat().fast().nutrition(1).effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED, 2000, 3), 1).build())));;
+	public static final RegistryObject<Item> URANIUM_INGOT = LuckyTNTMod.itemRegistry.register("uranium_ingot", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER)));
+	public static final RegistryObject<Item> ANTIMATTER = LuckyTNTMod.itemRegistry.register("antimatter", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER).rarity(Rarity.EPIC)) {
 		@Override
 		public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
 			super.appendHoverText(stack, level, components, flag);
 			components.add(Component.translatable("item.antimatter.info"));
 		}
 	});
-	public static final RegistryObject<Item> URANIUM_ORE = LuckyTNTMod.itemRegistry.register("uranium_ore", () -> new BlockItem(BlockRegistry.URANIUM_ORE.get(), new Item.Properties()));
-	public static final RegistryObject<Item> DEEPSLATE_URANIUM_ORE = LuckyTNTMod.itemRegistry.register("deepslate_uranium_ore", () -> new BlockItem(BlockRegistry.DEEPSLATE_URANIUM_ORE.get(), new Item.Properties()));
-	public static final RegistryObject<Item> GUNPOWDER_ORE = LuckyTNTMod.itemRegistry.register("gunpowder_ore", () -> new BlockItem(BlockRegistry.GUNPOWDER_ORE.get(), new Item.Properties()));
-	public static final RegistryObject<Item> DEEPSLATE_GUNPOWDER_ORE = LuckyTNTMod.itemRegistry.register("deepslate_gunpowder_ore", () -> new BlockItem(BlockRegistry.DEEPSLATE_GUNPOWDER_ORE.get(), new Item.Properties()));
-	public static final RegistryObject<Item> CONFIGURATION_WAND = LuckyTNTMod.itemRegistry.register("configuration_wand", () -> new Item(new Item.Properties().stacksTo(1)) {
+	public static final RegistryObject<Item> URANIUM_ORE = LuckyTNTMod.itemRegistry.register("uranium_ore", () -> new BlockItem(BlockRegistry.URANIUM_ORE.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)));
+	public static final RegistryObject<Item> DEEPSLATE_URANIUM_ORE = LuckyTNTMod.itemRegistry.register("deepslate_uranium_ore", () -> new BlockItem(BlockRegistry.DEEPSLATE_URANIUM_ORE.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)));
+	public static final RegistryObject<Item> GUNPOWDER_ORE = LuckyTNTMod.itemRegistry.register("gunpowder_ore", () -> new BlockItem(BlockRegistry.GUNPOWDER_ORE.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)));
+	public static final RegistryObject<Item> DEEPSLATE_GUNPOWDER_ORE = LuckyTNTMod.itemRegistry.register("deepslate_gunpowder_ore", () -> new BlockItem(BlockRegistry.DEEPSLATE_GUNPOWDER_ORE.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)));
+	public static final RegistryObject<Item> CONFIGURATION_WAND = LuckyTNTMod.itemRegistry.register("configuration_wand", () -> new Item(new Item.Properties().tab(LuckyTNTTabs.OTHER).stacksTo(1)) {
 		@Override
 		public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
 			super.appendHoverText(stack, level, components, flag);
 			components.add(Component.translatable("item.configuration_wand.info"));
 		}
 	});
-	public static final RegistryObject<Item> OBSIDIAN_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_RAIL.get(), new Item.Properties()) {
+	public static final RegistryObject<Item> OBSIDIAN_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_RAIL.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)) {
 		@Override
 		public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
 			super.appendHoverText(stack, level, components, flag);
 			components.add(Component.translatable("item.obsidian_rail.info"));
 		}
 	});
-	public static final RegistryObject<Item> OBSIDIAN_POWERED_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_powered_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_POWERED_RAIL.get(), new Item.Properties()) {
+	public static final RegistryObject<Item> OBSIDIAN_POWERED_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_powered_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_POWERED_RAIL.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)) {
 		@Override
 		public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
 			super.appendHoverText(stack, level, components, flag);
 			components.add(Component.translatable("item.obsidian_rail.info"));
 		}
 	});
-	public static final RegistryObject<Item> OBSIDIAN_ACTIVATOR_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_activator_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_ACTIVATOR_RAIL.get(), new Item.Properties()) {
+	public static final RegistryObject<Item> OBSIDIAN_ACTIVATOR_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_activator_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_ACTIVATOR_RAIL.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)) {
 		@Override
 		public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
 			super.appendHoverText(stack, level, components, flag);
 			components.add(Component.translatable("item.obsidian_rail.info"));
 		}
 	});
-	public static final RegistryObject<Item> OBSIDIAN_DETECTOR_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_detector_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_DETECTOR_RAIL.get(),new Item.Properties()) {
+	public static final RegistryObject<Item> OBSIDIAN_DETECTOR_RAIL = LuckyTNTMod.itemRegistry.register("obsidian_detector_rail", () -> new BlockItem(BlockRegistry.OBSIDIAN_DETECTOR_RAIL.get(),new Item.Properties().tab(LuckyTNTTabs.OTHER)) {
 		@Override
 		public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
 			super.appendHoverText(stack, level, components, flag);
@@ -223,5 +223,5 @@ public class ItemRegistry {
 	});
 	public static final RegistryObject<LDynamiteItem> DEATH_RAY_RAY = LuckyTNTMod.RH.registerDynamiteItem(LuckyTNTMod.itemRegistry, "death_ray_ray", () -> new DeathRayRayItem(), "none", false, true);
 	public static final RegistryObject<Item> VACUUM_CLEANER = LuckyTNTMod.itemRegistry.register("vacuum_cleaner", () -> new VacuumCleaner());
-	public static final RegistryObject<Item> TOXIC_STONE = LuckyTNTMod.itemRegistry.register("toxic_stone", () -> new BlockItem(BlockRegistry.TOXIC_STONE.get(), new Item.Properties()));
+	public static final RegistryObject<Item> TOXIC_STONE = LuckyTNTMod.itemRegistry.register("toxic_stone", () -> new BlockItem(BlockRegistry.TOXIC_STONE.get(), new Item.Properties().tab(LuckyTNTTabs.OTHER)));
 }
