b012b4d4bb2f551635d319687ead473a34a6a87a
Fleshcrafter <geissler.mael@gmail.com>
2023-07-08 13:35:06 +0200

    1.19.3-1.1

--- src/main/java/luckytnt/registry/CommandRegistry.java
+++ src/main/java/luckytnt/registry/CommandRegistry.java
@@ -6,7 +6,6 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import luckytnt.LuckyTNTMod;
 import luckytnt.commands.LTMDisastersCommand;
 import luckytnt.commands.RandomTNTCommand;
-import luckytnt.commands.RandomTNTCommandArgument;
 import net.minecraft.commands.Commands;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -29,7 +28,7 @@ public class CommandRegistry {
 		event.getDispatcher().register(Commands.literal("randomtnt").requires(s -> s.hasPermission(2))
 				
 				.then(Commands.literal("normal_tnt")
-				.then(Commands.argument("amount", RandomTNTCommandArgument.random()).executes((p) -> {
+				.then(Commands.argument("amount", IntegerArgumentType.integer(1)).executes((p) -> {
 					return RandomTNTCommand.executeGiveItems(p.getSource(), IntegerArgumentType.getInteger(p, "amount"), true, "n");
 			    })
 				.then(Commands.argument("allowDuplicate", BoolArgumentType.bool()).executes((p) -> {
@@ -37,7 +36,7 @@ public class CommandRegistry {
 				}))))
 				
 				.then(Commands.literal("dynamite")
-				.then(Commands.argument("amount", RandomTNTCommandArgument.random()).executes((p) -> {
+				.then(Commands.argument("amount", IntegerArgumentType.integer(1)).executes((p) -> {
 					return RandomTNTCommand.executeGiveItems(p.getSource(), IntegerArgumentType.getInteger(p, "amount"), true, "dy");
 				})
 				.then(Commands.argument("allowDuplicate", BoolArgumentType.bool()).executes((p) -> {
@@ -45,7 +44,7 @@ public class CommandRegistry {
 				}))))
 				
 				.then(Commands.literal("god_tnt")
-				.then(Commands.argument("amount", RandomTNTCommandArgument.random()).executes((p) -> {
+				.then(Commands.argument("amount", IntegerArgumentType.integer(1)).executes((p) -> {
 					return RandomTNTCommand.executeGiveItems(p.getSource(), IntegerArgumentType.getInteger(p, "amount"), true, "g");
 				})
 				.then(Commands.argument("allowDuplicate", BoolArgumentType.bool()).executes((p) -> {
@@ -53,7 +52,7 @@ public class CommandRegistry {
 				}))))
 				
 				.then(Commands.literal("doomsday_tnt")
-				.then(Commands.argument("amount", RandomTNTCommandArgument.random()).executes((p) -> {
+				.then(Commands.argument("amount", IntegerArgumentType.integer(1)).executes((p) -> {
 					return RandomTNTCommand.executeGiveItems(p.getSource(), IntegerArgumentType.getInteger(p, "amount"), true, "d");
 				})
 				.then(Commands.argument("allowDuplicate", BoolArgumentType.bool()).executes((p) -> {
