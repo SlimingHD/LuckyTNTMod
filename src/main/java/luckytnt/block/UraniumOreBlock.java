6f51cbdf5e59e7e29e62f79e1475fb55b26f6092
SlimingHD <94920232+SlimingHD@users.noreply.github.com>
2023-08-09 02:38:33 +0200

    1.2

--- src/main/java/luckytnt/block/UraniumOreBlock.java
+++ src/main/java/luckytnt/block/UraniumOreBlock.java
@@ -27,7 +27,7 @@ public class UraniumOreBlock extends Block{
 	@Override
     public boolean canHarvestBlock(BlockState state, BlockGetter world, BlockPos pos, Player player) {
         if (player.getInventory().getSelected().getItem() instanceof TieredItem tieredItem)
-            return tieredItem.getTier().getLevel() > 2;
+            return tieredItem.getTier().getLevel() >= 2;
         return false;
     }
 	
