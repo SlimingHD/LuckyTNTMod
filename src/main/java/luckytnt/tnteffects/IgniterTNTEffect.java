434381ff663ea491e24a7e78ffa01213b706f65d
Fleshcrafter <geissler.mael@gmail.com>
2024-03-27 17:53:58 +0100

    2.4

--- src/main/java/luckytnt/tnteffects/IgniterTNTEffect.java
+++ src/main/java/luckytnt/tnteffects/IgniterTNTEffect.java
@@ -1,15 +1,16 @@
 package luckytnt.tnteffects;
 
 import luckytnt.registry.BlockRegistry;
-import luckytntlib.block.LTNTBlock;
 import luckytntlib.util.IExplosiveEntity;
 import luckytntlib.util.explosions.ExplosionHelper;
 import luckytntlib.util.explosions.IForEachBlockExplosionEffect;
 import luckytntlib.util.tnteffects.PrimedTNTEffect;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.TntBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class IgniterTNTEffect extends PrimedTNTEffect{
@@ -26,8 +27,8 @@ public class IgniterTNTEffect extends PrimedTNTEffect{
 			
 			@Override
 			public void doBlockExplosion(Level level, BlockPos pos, BlockState state, double distance) {
-				if(state.getBlock() instanceof LTNTBlock) {
-					((LTNTBlock)state.getBlock()).explode(level, false, pos.getX(), pos.getY(), pos.getZ(), entity.owner() instanceof LivingEntity ? (LivingEntity)entity.owner() : null);
+				if(state.getBlock() instanceof TntBlock block) {
+					block.onBlockExploded(state, level, pos, new Explosion(level, (Entity)entity, null, null, pos.getX(), pos.getY(), pos.getZ(), 0, false, Explosion.BlockInteraction.DESTROY));
 				}
 			}
 		});
